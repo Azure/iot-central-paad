9c9c44cf48031d08615ebe2b4fe86fe296c36a34
lucadruda <ludruda@microsoft.com>
2021-01-18 15:37:21 +0100

    fix module paths

--- src/Insight.tsx
+++ src/Insight.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { View, StyleSheet, processColor } from 'react-native';
-import { LineChart } from 'react-native-charts-wrapper';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import {View, StyleSheet, processColor} from 'react-native';
+import {LineChart} from 'react-native-charts-wrapper';
 import {
   getRandomColor,
   Text,
@@ -11,50 +11,63 @@ import {
   ExtendedLineData,
   ItemData,
   CustomLineDatasetConfig,
-  NavigationParams, DATA_AVAILABLE_EVENT
+  NavigationParams,
+  DATA_AVAILABLE_EVENT,
 } from './types';
-import { useTheme, RouteProp } from '@react-navigation/native';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
-import { useScreenDimensions } from './hooks/layout';
-import { Loader } from './components/loader';
+import {useTheme, RouteProp} from '@react-navigation/native';
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
+import {useScreenDimensions} from './hooks/layout';
+import {Loader} from './components/loader';
 import Map from './components/map';
-import { HealthMap, SensorMap } from './sensors';
-
+import {HealthMap, SensorMap} from './sensors';
 
 export enum ChartType {
   DEFAULT,
-  MAP
+  MAP,
 }
 
-const Insight = React.memo<{ route: RouteProp<Record<string, NavigationParams & { chartType: ChartType, telemetryId: string; currentValue: any }>, 'Insight'> }>(({ route }) => {
-  const { screen } = useScreenDimensions();
-  const { colors, dark } = useTheme();
+const Insight = React.memo<{
+  route: RouteProp<
+    Record<
+      string,
+      NavigationParams & {
+        chartType: ChartType;
+        telemetryId: string;
+        currentValue: any;
+      }
+    >,
+    'Insight'
+  >;
+}>(({route}) => {
+  const {screen} = useScreenDimensions();
+  const {colors, dark} = useTheme();
   const [data, setData] = useState<ExtendedLineData>({
     dataSets: [],
   });
-  const { current: start } = useRef(Date.now());
+  const {current: start} = useRef(Date.now());
   const timestamp = Date.now() - start;
-  const { chartType, telemetryId, currentValue } = route.params;
+  const {chartType, telemetryId, currentValue} = route.params;
 
-  const mapStyle = useMemo(() => (
-    {
+  const mapStyle = useMemo(
+    () => ({
       flex: 3,
       margin: 20,
       borderRadius: 20,
       ...(!dark
         ? {
-          shadowColor: "'rgba(0, 0, 0, 0.14)'",
-          shadowOffset: {
-            width: 0,
-            height: 3,
-          },
-          shadowOpacity: 0.8,
-          shadowRadius: 3.84,
-          elevation: 5,
-        }
+            shadowColor: "'rgba(0, 0, 0, 0.14)'",
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowOpacity: 0.8,
+            shadowRadius: 3.84,
+            elevation: 5,
+          }
         : {}),
-    }), [dark]);
-
+    }),
+    [dark],
+  );
 
   /**
    *
@@ -64,7 +77,7 @@ const Insight = React.memo<{ route: RouteProp<Record<string, NavigationParams & 
    */
   const updateData = useCallback(
     (id: string, value: any) => {
-      const item = { id, value };
+      const item = {id, value};
       if (id !== telemetryId) {
         return;
       }
@@ -82,7 +95,7 @@ const Insight = React.memo<{ route: RouteProp<Record<string, NavigationParams & 
             (d) => d.itemId === itemdata.id,
           );
           // Current sample time (x-axis) is the difference between current timestamp e the start time of sampling
-          const newSample = { x: Date.now() - start, y: itemdata.value };
+          const newSample = {x: Date.now() - start, y: itemdata.value};
 
           if (!currentItemData) {
             // current item is not in the dataset yet
@@ -106,12 +119,15 @@ const Insight = React.memo<{ route: RouteProp<Record<string, NavigationParams & 
               ],
             };
           }
-          if (currentItemData.values?.[currentItemData.values?.length] === itemdata.value) {
-            return { ...currentDataSet };
+          if (
+            currentItemData.values?.[currentItemData.values?.length] ===
+            itemdata.value
+          ) {
+            return {...currentDataSet};
           }
           return {
             ...currentDataSet,
-            dataSets: currentDataSet.dataSets.map(({ ...currentItem }) => {
+            dataSets: currentDataSet.dataSets.map(({...currentItem}) => {
               if (currentItem.itemId === itemdata.id && currentItem.values) {
                 currentItem.values = [...currentItem.values, ...[newSample]];
               }
@@ -131,16 +147,15 @@ const Insight = React.memo<{ route: RouteProp<Record<string, NavigationParams & 
     if (currentValue !== undefined) {
       updateData(telemetryId, currentValue);
     }
-    return () => { sensor?.removeListener(DATA_AVAILABLE_EVENT, updateData) }
+    return () => {
+      sensor?.removeListener(DATA_AVAILABLE_EVENT, updateData);
+    };
   }, [telemetryId, currentValue, updateData]);
 
   if (chartType === ChartType.MAP) {
     return (
       <View style={style.container}>
-        <Map
-          style={mapStyle}
-          location={currentValue}
-        />
+        <Map style={mapStyle} location={currentValue} />
         <View style={style.summary}>
           <Text>
             <Name>Latitude:</Name> {currentValue.lat}
@@ -158,94 +173,94 @@ const Insight = React.memo<{ route: RouteProp<Record<string, NavigationParams & 
       {data.dataSets.length === 0 ? (
         <Loader message="" visible={true} />
       ) : (
-          <View style={style.container}>
-            <View style={style.chart}>
-              <LineChart
-                style={style.chartBox}
-                chartDescription={{ text: '' }}
-                touchEnabled={true}
-                dragEnabled={true}
-                scaleEnabled={true}
-                pinchZoom={true}
-                extraOffsets={{ bottom: 20 }}
-                legend={{
-                  wordWrapEnabled: true,
-                  textColor: processColor(colors.text),
-                  textSize: 16,
-                }}
-                xAxis={{
-                  position: 'BOTTOM',
-                  axisMaximum: timestamp + 500,
-                  axisMinimum: timestamp - 10000,
-                  valueFormatter: 'date',
-                  since: start,
-                  valueFormatterPattern: 'HH:mm:ss',
-                  timeUnit: 'MILLISECONDS',
+        <View style={style.container}>
+          <View style={style.chart}>
+            <LineChart
+              style={style.chartBox}
+              chartDescription={{text: ''}}
+              touchEnabled={true}
+              dragEnabled={true}
+              scaleEnabled={true}
+              pinchZoom={true}
+              extraOffsets={{bottom: 20}}
+              legend={{
+                wordWrapEnabled: true,
+                textColor: processColor(colors.text),
+                textSize: 16,
+              }}
+              xAxis={{
+                position: 'BOTTOM',
+                axisMaximum: timestamp + 500,
+                axisMinimum: timestamp - 10000,
+                valueFormatter: 'date',
+                since: start,
+                valueFormatterPattern: 'HH:mm:ss',
+                timeUnit: 'MILLISECONDS',
+                drawAxisLines: false,
+                drawGridLines: false,
+                textColor: processColor(colors.text),
+              }}
+              yAxis={{
+                right: {
                   drawAxisLines: false,
-                  drawGridLines: false,
                   textColor: processColor(colors.text),
-                }}
-                yAxis={{
-                  right: {
-                    drawAxisLines: false,
-                    textColor: processColor(colors.text),
-                  },
-                  left: {
-                    drawAxisLines: false,
-                    textColor: processColor(colors.text),
-                  },
-                }}
-                data={data}
-              />
-              <View style={style.summary}>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-around',
-                    marginTop: 40,
-                  }}>
-                  {data.dataSets.map((d, i) => {
-                    if (!d.values) {
-                      return null;
-                    }
-                    const val = (d.values[d.values.length - 1] as {
-                      x: any;
-                      y: any;
-                    }).y;
-                    const fill = val > 1 || val < -1 ? val : Math.abs(val * 1000);
-                    return (
-                      <AnimatedCircularProgress
-                        key={`circle-${i}`}
-                        size={screen.width / 5}
-                        width={5}
-                        fill={fill}
-                        tintColor={
-                          d.config ? d.config.rgbcolor : getRandomColor()
-                        }
-                        backgroundColor={
-                          d.config
-                            ? LightenDarkenColor(d.config.rgbcolor, 90, true)
-                            : getRandomColor()
-                        }
-                        rotation={360}>
-                        {() => {
-                          const strVal = `${val}`;
-                          return (
-                            <Text>
-                              {strVal.length > 6
-                                ? `${strVal.substring(0, 6)}...`
-                                : strVal}
-                            </Text>
-                          );
-                        }}
-                      </AnimatedCircularProgress>
-                    );
-                  })}
-                </View>
+                },
+                left: {
+                  drawAxisLines: false,
+                  textColor: processColor(colors.text),
+                },
+              }}
+              data={data}
+            />
+            <View style={style.summary}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-around',
+                  marginTop: 40,
+                }}>
+                {data.dataSets.map((d, i) => {
+                  if (!d.values) {
+                    return null;
+                  }
+                  const val = (d.values[d.values.length - 1] as {
+                    x: any;
+                    y: any;
+                  }).y;
+                  const fill = val > 1 || val < -1 ? val : Math.abs(val * 1000);
+                  return (
+                    <AnimatedCircularProgress
+                      key={`circle-${i}`}
+                      size={screen.width / 5}
+                      width={5}
+                      fill={fill}
+                      tintColor={
+                        d.config ? d.config.rgbcolor : getRandomColor()
+                      }
+                      backgroundColor={
+                        d.config
+                          ? LightenDarkenColor(d.config.rgbcolor, 90, true)
+                          : getRandomColor()
+                      }
+                      rotation={360}>
+                      {() => {
+                        const strVal = `${val}`;
+                        return (
+                          <Text>
+                            {strVal.length > 6
+                              ? `${strVal.substring(0, 6)}...`
+                              : strVal}
+                          </Text>
+                        );
+                      }}
+                    </AnimatedCircularProgress>
+                  );
+                })}
               </View>
             </View>
           </View>
-        )}
+        </View>
+      )}
     </>
   );
 });
