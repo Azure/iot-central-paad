2a0bd5e672d04f01d212280544c79e9b6e74103c
lucadruda <lucadruda@gmail.com>
2022-06-13 15:52:37 +0200

    update build pipelines

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
  * @format
  */
 
-import { AppRegistry, Platform } from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './src/App';
-import { name as appName } from './app.json';
-if (Platform.OS === 'android') { // only android needs polyfill
-    require('intl'); // import intl object
-    require('intl/locale-data/jsonp/en-US'); // load the required locale details
+import {name as appName} from './app.json';
+if (Platform.OS === 'android') {
+  // only android needs polyfill
+  require('intl'); // import intl object
+  require('intl/locale-data/jsonp/en-US'); // load the required locale details
 }
 
 AppRegistry.registerComponent(appName, () => App);
